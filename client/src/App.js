da3423a897a22f7ba71c35e39bf8b20441ed1e90
travis scott <ellevis54@yahoo.com>
2019-09-27 22:09:46 -0700

    fix call to local host

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ class App extends React.Component {
     handleRegister = async (formData) => {
       console.log("REGISTERING")
       console.log(formData);
-      const registerResponse = await fetch("http://localhost:9000/auth/register",{
+      const registerResponse = await fetch("/auth/register",{
         method: "POST",
         body: JSON.stringify(formData),
         credentials: "include",
@@ -39,7 +39,7 @@ class App extends React.Component {
     handleLogin = async (formData) => {
       console.log("REGISTERING")
       console.log(formData);
-      const registerLogin = await fetch("http://localhost:9000/auth/login",{
+      const registerLogin = await fetch("/auth/login",{
         method: "POST",
         body: JSON.stringify(formData),
         credentials: "include",
